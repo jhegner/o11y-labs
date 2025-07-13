dbc302a5af1112bb39172738f8b389bbce7edb62
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-07-13 02:25:27 -0300

    chore: novas apps e outros arquivos

---
+++ datadog/o11y-labs-app3-jms/src/test/java/br/com/jhegner/o11ylabs/app3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhegner.o11ylabs.app3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
